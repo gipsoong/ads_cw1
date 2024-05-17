8a15bf9c10f92d3844528d76c211cb69ec656da0
gipsoong <ryangipsoong@gmail.com>
2024-05-18 02:36:00 +0800

    Initialise class attributes

--- property.py
+++ property.py
@@ -3,5 +3,12 @@
 class Property:
     def __init__(self, postal_code, tenure, completion_year, property_type, area, commission_rate,
                  valuation):
+        self.postal_code = postal_code
+        self.tenure = tenure
+        self.completion_year = completion_year
+        self.property_type = property_type
+        self.area = area
+        self.commission_rate = commission_rate
+        self.valuation = valuation
         return
 
