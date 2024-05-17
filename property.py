07512713715cc01bb6dd536543fa657da4993949
gipsoong <ryangipsoong@gmail.com>
2024-05-17 23:56:39 +0800

    Create 'Property' class and initialise its paremeters

--- property.py
+++ property.py
@@ -1,4 +1,7 @@
-print("Hello World!")
-print("BYE World!")
-print('Hi')
-print('Test Checkout')
+
+
+class Property:
+    def __init__(self, postal_code, tenure, completion_year, property_type, area, commission_rate,
+                 valuation):
+        return
+
