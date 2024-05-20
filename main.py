177e5272d93e18f9c0c39579579a197f988145a9
gipsoong <ryangipsoong@gmail.com>
2024-05-20 17:41:59 +0800

    Create 'main' file for testing purposes. Import PropertyAgent class and create an object based on it named 'agent_john'.

---
+++ main.py
@@ -0,0 +1,6 @@
+from propertyAgent import PropertyAgent
+
+def main():
+    """Start of program."""
+    agent_john = PropertyAgent(["1", "2", "3"], ["4", "5", "6"], "NexProp",
+                               100901, "2005")
