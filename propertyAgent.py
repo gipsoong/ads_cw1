05b49a4088e3438d651fe9e5ff75dcb60b257cf0
gipsoong <ryangipsoong@gmail.com>
2024-05-20 17:23:14 +0800

    Added additional attributes to PropertyAgent class

--- propertyAgent.py
+++ propertyAgent.py
@@ -1,10 +1,11 @@
 
 
 class PropertyAgent:
+    agent_comm_sharing_rate = 0.7
 
-    def __init__(self, not_sold, sold):
+    def __init__(self, not_sold, sold, company, reg_num, year_start):
         self.not_sold = not_sold
         self.sold = sold
-
-
-agent_comm_sharing_rate = 0.7
+        self.company = company
+        self.reg_num = reg_num
+        self.year_start = year_start
