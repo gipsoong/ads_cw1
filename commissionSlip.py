a7dc58b9a34e6ef5cc875fce34b0f42c54a5115f
gipsoong <ryangipsoong@gmail.com>
2024-05-21 00:22:27 +0800

    Rename CalculateCommission class to CommissionSlip. Initialize attributes of CommissionSlip.

--- commissionSlip.py
+++ commissionSlip.py
@@ -1,7 +1,10 @@
 
 
-class CalculateCommission:
-    """Initialise the attributes of "CalculateCommission"""
+class CommissionSlip:
+    """Initialise the attributes of "CommissionSlip"""
     # CalculateCommission calculates the annual commission earned by an agent/director
-    def __init__(self):
-        pass
+    def __init__(self, total_sold, commission_rate, commission_sharing_rate):
+        self.total_sold = total_sold
+        self.commission_rate = commission_rate
+        self.commission_sharing_rate = commission_sharing_rate
+
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from propertyAgent import PropertyAgent
 
+
 def main():
     """Start of program."""
     agent_john = PropertyAgent(["1", "2", "3"], ["4", "5", "6"], "NexProp",
                                100901, "2005")
 
     print(agent_john)
-    print(agent_john.not_sold)
-    print(agent_john.agent_commission_rate)
-    print(agent_john.agent_commission_sharing_rate)
 
-main() # start program.
+
+main()  # start program.
