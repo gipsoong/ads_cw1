a62e391dd84cc89836d1bf30cc49c842560ff1cd
gipsoong <ryangipsoong@gmail.com>
2024-05-28 03:41:06 +0800

    Hi

--- commissionSlip.py
+++ commissionSlip.py
@@ -7,13 +7,18 @@ class CommissionSlip:
 
     def calculate_agent_commission(self):
         comm_sharing_rate = self.x.commission_sharing_rate
+        total_commission = 0
         print(f"----------\n"
               f"COMMISSION SLIP of Property Agent {self.x.name}:")
         for i in self.x.sold:
+            total_commission += (i.valuation * i.commission_rate) * comm_sharing_rate
+
             print(f"- {i.address}, transacted at P${i.valuation:,.2f}."
                   f" Net commission earned = P${((i.valuation * i.commission_rate) * comm_sharing_rate):,.2f}\n"
                   f"({i.valuation:,.2f} * {i.commission_rate} * {comm_sharing_rate} ="
                   f" {((i.valuation * i.commission_rate) * comm_sharing_rate):,.2f})")
+
+        print(f"TOTAL COMMISSION: P${total_commission:,.2f}")
 
     def calculate_director_commission(self):
         comm_sharing_rate = self.x.commission_sharing_rate
